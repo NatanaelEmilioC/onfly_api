326352074fe7655012b80be582f9d3441b645b5b
Natanael Costa <info.natanael@unidas.com.br>
2022-07-31 17:23:47 -0300

    cofigurando docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { ExpenseModule } from './modules/expense.module';
 import { UserModule } from './modules/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const settings = require('../ormconfig.js');
 
 @Module({
-  imports: [ExpenseModule, UserModule, TypeOrmModule.forRoot()],
+  imports: [
+    ExpenseModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(settings),
+  ],
 })
 export class AppModule {}
